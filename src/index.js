e23d2331e0476f92fed4b985c4480dd5f4670a3c
Anastasia Shapovalova <shabalinastya@gmail.com>
2018-10-31 22:33:02 +0000

    2nd homework for the first chekk

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
+  for(var i=0; i<array.length; i++){
+    fn(array[i], i, array);
+  }
 }
 
 /*
@@ -16,6 +19,11 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+  var newArr = [];
+  for(var i=0; i<array.length; i++){
+    newArr.push(fn(array[i], i, array));  
+  }
+  return newArr;
 }
 
 /*
@@ -25,6 +33,13 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
+  var init = initial ? 0 : 1;
+  initial = initial || array[0];
+
+  for(var i=init; i<array.length; i++){
+    initial = fn(initial, array[i], i, array);
+  }
+  return initial;
 }
 
 /*
@@ -36,6 +51,8 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+  var allNamesUp = Object.getOwnPropertyNames(obj).join(',').toUpperCase();
+  return allNamesUp.split(',');  
 }
 
 /*
@@ -44,7 +61,17 @@ function upperProps(obj) {
  Напишите аналог встроенного метода slice для работы с массивами
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
-function slice(array, from, to) {
+function slice(array, from = 0, to = array.length) {  
+  var slicedArr = [];
+  to = to >= 0 ? to : (array.length + to);
+  to = to <= array.length ? to : array.length;
+  if (from<0){
+    from = from <(-array.length) ? 0 : array.length + from;
+    }
+  for(var i=from; i<to; i++){
+    slicedArr.push(array[i]);
+  }
+  return slicedArr;  
 }
 
 /*
@@ -54,6 +81,15 @@ function slice(array, from, to) {
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
 function createProxy(obj) {
+  var handler = {
+    set: function(obj, prop, value) {
+      if (!Number.isInteger(value)) {
+          throw new TypeError('The value is not an integer');
+      }  
+      obj[prop] = value*value;       
+    }    
+  }
+  return new Proxy(obj, handler); 
 }
 
 export {
